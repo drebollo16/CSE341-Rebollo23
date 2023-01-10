89e3823676863a7660a0214c6d6f219f4b8ac1a3
drebollo16 <drebollo16@byui.edu>
2023-01-10 13:32:12 -0700

    changes monogoDB - testing

---
+++ DB/connection.js
@@ -0,0 +1,13 @@
+const { MongoClient } = require('mongodb');
+
+async function main() {
+    // we'll add code here soon
+}
+
+/**
+ * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+ * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+ */
+const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
