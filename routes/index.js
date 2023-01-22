0275a3f4129be08360df909d9c140e80f687a236
drebollo16 <drebollo16@byui.edu>
2023-01-21 22:18:39 -0700

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
-const routes = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const contactController = require('../controllers/contacts');
 
-
+/*
 routes.get('/', (req, res) => {
     res.send('Denise rebollo');
-});
+});*/
 
 
 
 //routes.get('/contacts');
 
-routes.use('/contacts', require('./contacts'));
+router.use('/contacts', require('./contacts'));
 
 //added to get contacts by ID
 //routes.get('/: id ', contactController.getOneId);
 
-module.exports = routes;
+module.exports = router;
